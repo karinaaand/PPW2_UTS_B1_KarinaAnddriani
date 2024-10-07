6be2911fd785522f7e790ee44ee4865d8734da59
karinaaand <karinaandriani@mail.ugm.ac.id>
2024-10-08 05:47:32 +0700

    crudlaravel

---
+++ resources/views/pemain/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Data Pemain</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama Pemain</th>
+                <th>No Punggung</th>
+                <th>Posisi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($pemain as $p)
+            <tr>
+                <td>{{ $p->id }}</td>
+                <td>{{ $p->nama_pemain }}</td>
+                <td>{{ $p->no_punggung }}</td>
+                <td>{{ $p->posisi }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
